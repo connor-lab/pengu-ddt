8be5a7464a193967bdab921cabd25b66b6386d28
Matt Bull <bullmj2@gmail.com>
2019-06-24 17:23:27 +0100

    Refactor clustercode to UPDATE not INSERT if code exists

--- penGU/snpaddress/clustercode_database.py
+++ penGU/snpaddress/clustercode_database.py
@@ -10,8 +10,7 @@ from penGU.input.utils import check_config_file
 
 def retrieve_snp_addresses_from_snapperdb(snapperdb_config_dict):
     """ This function queries a snapperdb database and
-    returns a dict of {{tXX : cluster} : frequency} to 
-    be filtered for singletons later"""
+    returns a dict of {{tXX : cluster} : frequency}"""
 
     NGSdb = NGSDatabase(snapperdb_config_dict)
     conn = NGSdb._connect_to_db()
@@ -59,25 +58,39 @@ def update_clustercode_database(config_dict, snapperdb_conf):
 
     try:
         for row in insert_dict_list:
-            cur.execute("""INSERT INTO clustercode_snpaddress
-                       (clustercode,
-                       clustercode_frequency, 
-                       reference_name, 
-                       t250,
-                       t100,
-                       t50,
-                       t25,
-                       t10,
-                       t5,
-                       t2,
-                       t0,
-                       snpaddress_string,
-                       clustercode_updated)
-                       VALUES (%(clustercode)s, %(clustercode_frequency)s, 
-                       %(reference_name)s, %(t250)s, %(t100)s, %(t50)s, %(t25)s, 
-                       %(t10)s, %(t5)s, %(t2)s, %(t0)s, %(snpaddress_string)s, 
-                       %(clustercode_updated)s);
-                    """, row)
+            ## Does snpaddress exist in DB? If yes UPDATE if no INSERT
+            cur.execute("""SELECT id FROM clustercode_snpaddress WHERE 
+                           snpaddress_string = %(snpaddress_string)s 
+                           AND reference_name = %(reference_name)s""", (row))
+            
+            if cur.fetchone() is not None:
+                print("Updating SNPaddress freqency to " + str(row["clustercode_frequency"]) + " for " + row["clustercode"])
+                cur.execute("""UPDATE clustercode_snpaddress
+                            SET clustercode_frequency = %(clustercode_frequency)s,
+                            clustercode_updated = %(clustercode_updated)s
+                            WHERE snpaddress_string = %(snpaddress_string)s 
+                            AND reference_name = %(reference_name)s""", (row))
+            
+            elif cur.fetchone() is None:
+                cur.execute("""INSERT INTO clustercode_snpaddress
+                        (clustercode,
+                        clustercode_frequency, 
+                        reference_name, 
+                        t250,
+                        t100,
+                        t50,
+                        t25,
+                        t10,
+                        t5,
+                        t2,
+                        t0,
+                        snpaddress_string,
+                        clustercode_updated)
+                        VALUES (%(clustercode)s, %(clustercode_frequency)s, 
+                        %(reference_name)s, %(t250)s, %(t100)s, %(t50)s, %(t25)s, 
+                        %(t10)s, %(t5)s, %(t2)s, %(t0)s, %(snpaddress_string)s, 
+                        %(clustercode_updated)s);
+                        """, row)
         conn.commit()
         cur.close()
         conn.close()
