af46dc7363de83a158f305be9ab72170e33cb74e
Matt Bull <bullmj2@gmail.com>
2019-06-25 16:06:22 +0100

    Finalise pip installable

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 import setuptools
+import pip
 
 with open("VERSION", 'r') as v:
     version = v.readline()
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+
+if int(pip.__version__.split(".")[0]) >= 6:
+    install_reqs = parse_requirements('requirements.txt', session=False)
+else:
+    install_reqs = parse_requirements('requirements.txt')
+
+install_requires = [str(ir.req) for ir in install_reqs]
 
 setuptools.setup(
     name='ddt',
@@ -10,5 +24,6 @@ setuptools.setup(
     author_email="Matthew.Bull@Wales.nhs.uk",
     description="A package for interacting with the DIGEST database",
     packages=setuptools.find_packages(),
-    scripts=["ddt.py"]
+    scripts=["ddt.py"],
+    install_requires=install_requires
     )
