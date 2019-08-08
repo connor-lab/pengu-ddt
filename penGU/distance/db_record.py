dcde72388edf811cd8799a760175880ee31b24ea
Matt Bull <bullmj2@gmail.com>
2019-08-08 17:40:00 +0100

    Remove file extensions from distance data

--- penGU/distance/db_record.py
+++ penGU/distance/db_record.py
@@ -8,8 +8,8 @@ from penGU.input.utils import read_data_from_csv
 def parse_dist_csv(dist_dict):
     for row in dist_dict:
         row["reference_name"] = os.path.splitext(os.path.basename(row["reference_name"]))[0]
+        row["y_number"] = os.path.splitext(os.path.basename(row["y_number"]))[0]
         if row["y_number"].startswith("DIGCD"):
-            row["y_number"] = os.path.splitext(os.path.basename(row["y_number"]))[0]
             m = re.search('DIGCD-(.*)_S\\d+$', row["y_number"])
             row["y_number"] = m.group(1)
     return dist_dict
@@ -21,7 +21,7 @@ def update_distance_database(config_dict, dist_csv):
                  "reference_mash_p_value",
                  "reference_common_kmers"]
     dist_data = read_data_from_csv(dist_csv, header=dist_cols)
-    dist_data_clean = parse_dist_csv(dist_data)
+    dist_data_clean = parse_dist_csv(dist_data)    
 
     NGSdb = NGSDatabase(config_dict)
     conn = NGSdb._connect_to_db()
